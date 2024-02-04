92f6c82b9e37ff5cf943491bd2515e0f50083946
SSStuart <71145823+SSStuart@users.noreply.github.com>
2024-02-04 22:41:21 +0100

    Ajoutez des fichiers projet.

---
+++ EntryPoint.cs
@@ -0,0 +1,69 @@
+﻿using Rage;
+using RAGENativeUI;
+using RAGENativeUI.Elements;
+using System.Drawing;
+
+[assembly: Rage.Attributes.Plugin("Health & Armour Display", Description = "A plugin displaying the player's life and armour with an icon and its value", Author = "SSStuart")]
+
+
+namespace HealthArmourDisplay
+{
+    public class EntryPoint
+    {
+        public static string pluginName = "Health & Armour Display";
+        public static string pluginVersion = "v 0.0.1";
+
+        public static void Main()
+        {
+            Game.LogTrivial(pluginName + " loaded.");
+
+            Settings.LoadSettings();
+            Game.LogTrivial("[" + pluginName + "] Plugin settings loaded.");
+
+            GameFiber.StartNew(delegate
+            {
+                Color healthColor = Color.LightCoral;
+                Color armourColor = Color.DeepSkyBlue;
+
+                // Waiting for the player to "exist"
+                while (!Game.LocalPlayer.Character.IsOnScreen)
+                {
+                    GameFiber.Yield();
+                }
+                Ped player = Game.LocalPlayer.Character;
+                int playerMaxHealth = player.MaxHealth - 100;
+                int screenHeight = Game.Resolution.Height;
+                int screenWidth = Game.Resolution.Width;
+                Point offset = new Point(Settings.BaseOffsetHorizontal, screenHeight - Settings.BaseOffsetVertical);
+
+                while (true)
+                {
+                    GameFiber.Yield();
+
+
+                    if (player.IsAlive)
+                    {
+                        // CIRCLE MINIMAP
+                        // Health
+                        ResText.Draw(((player.Health-100)*100/playerMaxHealth).ToString(), new Point(offset.X + Settings.HealthTextHorizontal, offset.Y + Settings.HealthTextVertical), 0.3f, healthColor, Common.EFont.Pricedown, false);
+                        Sprite.Draw("commonmenu", "shop_health_icon_b", new Point(offset.X + Settings.HealthIconHorizontal, offset.Y + Settings.HealthIconVertical), new Size(50, 50), 0f, healthColor);
+
+                        //Armour
+                        Sprite.Draw("commonmenu", "shop_armour_icon_b", new Point(offset.X + Settings.ArmourIconHorizontal, offset.Y + Settings.ArmourIconVertical), new Size(50, 50), 0f, armourColor);
+                        ResText.Draw(player.Armor.ToString(), new Point(offset.X + Settings.ArmourTextHorizontal, offset.Y + Settings.ArmourTextVertical), 0.3f, armourColor, Common.EFont.Pricedown, true);
+
+
+                        // SQUARE MINIMAP
+                        /*Sprite.Draw("commonmenu", "bettingbox_centre", new Point(offset.X - 40, offset.Y + 4), new Size(270, 50), 0f, Color.FromArgb(100, Color.Black));
+
+                        Sprite.Draw("commonmenu", "shop_health_icon_b", offset, new Size(50, 50), 0f, Color.LightCoral);
+                        ResText.Draw((player.Health-100).ToString(), new Point(offset.X + 60, offset.Y + 12), 0.3f, Color.LightCoral, Common.EFont.ChaletLondon, true);
+
+                        Sprite.Draw("commonmenu", "shop_armour_icon_b", new Point(offset.X + 100, offset.Y), new Size(50, 50), 0f, Color.LightBlue);
+                        ResText.Draw(player.Armor.ToString(), new Point(offset.X + 160, offset.Y + 12), 0.3f, Color.LightBlue, Common.EFont.ChaletLondon, true);*/
+                    }
+                }
+            });
+        }
+    }
+}
---
+++ IniFile.cs
@@ -0,0 +1,40 @@
+﻿using Rage;
+
+namespace HealthArmourDisplay
+{
+    internal static class Settings
+    {
+        internal static int BaseOffsetVertical = 45;
+        internal static int BaseOffsetHorizontal = 65;
+        internal static int HealthTextVertical = 15;
+        internal static int HealthTextHorizontal = 0;
+        internal static int HealthIconVertical = 0;
+        internal static int HealthIconHorizontal = 30;
+        internal static int ArmourTextVertical = 15;
+        internal static int ArmourTextHorizontal = 260;
+        internal static int ArmourIconVertical = 0;
+        internal static int ArmourIconHorizontal = 200;
+
+
+
+        internal static string path = "Plugins/HealthArmourDisplay.ini";
+        internal static InitializationFile ini = new InitializationFile(path);
+
+        internal static void LoadSettings()
+        {
+            Game.LogTrivial("[LOG]: Loading config file for " + EntryPoint.pluginName + ".");
+            
+            ini.Create();
+            BaseOffsetVertical = ini.ReadInt16("BasePosition", "BaseOffsetVertical", 45);
+            BaseOffsetHorizontal = ini.ReadInt16("BasePosition", "BaseOffsetHorizontal", 65);
+            HealthTextVertical = ini.ReadInt16("HealthTextPosition", "HealthTextVertical", 15);
+            HealthTextHorizontal = ini.ReadInt16("HealthTextPosition", "HealthTextHorizontal", 0);
+            HealthIconVertical = ini.ReadInt16("HealthIconPosition", "HealthIconVertical", 0);
+            HealthIconHorizontal = ini.ReadInt16("HealthIconPosition", "HealthIconHorizontal", 30);
+            ArmourTextVertical = ini.ReadInt16("ArmourTextPosition", "ArmourTextVertical", 15);
+            ArmourTextHorizontal = ini.ReadInt16("ArmourTextPosition", "ArmourTextHorizontal", 260);
+            ArmourIconVertical = ini.ReadInt16("ArmourIconPosition", "ArmourIconVertical", 0);
+            ArmourIconHorizontal = ini.ReadInt16("ArmourIconPosition", "ArmourIconHorizontal", 200);
+        }
+    }
+}
